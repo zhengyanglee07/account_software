5d3319930777bc0d580b79e681649d848837fb18
zhengyanglee07 <zhengyangz1007@gmail.com>
2025-01-10 15:25:24 +0800

    fix: update migration

---
+++ database/migrations/2025_01_10_133304_add_columns_into_processed_contacts_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsIntoProcessedContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('processed_contacts', function (Blueprint $table) {
+            $table->string('reg_no')->nullable();
+            $table->string('old_reg_no')->nullable();
+            $table->string('sst_reg_no')->nullable();
+            $table->string('reg_no_type')->nullable();
+            $table->string('tax_id_no')->nullable();
+            $table->text('remarks')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('processed_contacts', function (Blueprint $table) {
+            $table->dropColumn('reg_no');
+            $table->dropColumn('old_reg_no');
+            $table->dropColumn('sst_reg_no');
+            $table->dropColumn('reg_no_type');
+            $table->dropColumn('tax_id_no');
+            $table->dropColumn('remarks');
+        });
+    }
+}
---
+++ database/migrations/2025_01_10_134240_add_columns_into_processed_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsIntoProcessedAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('processed_addresses', function (Blueprint $table) {
+            $table->string('country_code')->nullable();
+            $table->tinyInteger('is_default_billing')->default(0);
+            $table->tinyInteger('is_default_shipping')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('processed_addresses', function (Blueprint $table) {
+            $table->dropColumn('country_code');
+            $table->dropColumn('is_default_billing');
+            $table->dropColumn('is_default_shipping');
+        });
+    }
+}
