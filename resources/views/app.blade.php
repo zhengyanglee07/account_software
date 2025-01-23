54c25ab9d6ce331e119597935e48bfd4e0c25985
zhengyanglee07 <zhengyangz1007@gmail.com>
2025-01-23 17:38:33 +0800

    fix: remove hyperhapes favicn

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,32 +5,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link 
-      rel="preconnect" 
+    <link
+      rel="preconnect"
       href="https://fonts.googleapis.com">
-      
-    <link 
-      rel="preconnect" 
-      href="https://fonts.gstatic.com" 
+
+    <link
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
       crossorigin>
 
-    <link 
-      rel="stylesheet" 
-      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" 
-      integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" 
-      crossorigin="anonymous" 
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+      integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+      crossorigin="anonymous"
       referrerpolicy="no-referrer" />
-    
-    <link 
-      rel="stylesheet" 
+
+    <link
+      rel="stylesheet"
       href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 
-    <link 
-      rel="shortcut icon" 
-      href="https://media.hypershapes.com/images/hypershapes-favicon.png" />
-    
+    <link
+      rel="shortcut icon"
+      href="https://picsum.photos/seed/picsum/50/50" />
+
     @vite('resources/app.js')
-      
+
     @production
       <!-- Google Tag Manager -->
       <script>
@@ -43,7 +43,7 @@
         window.fwSettings={
         'widget_id':72000001684
         };
-        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}() 
+        !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
       </script>
       <script type='text/javascript' src='https://widget.freshworks.com/widgets/72000001684.js' async defer></script>
       {{-- End of Freshdesk widget --}}
